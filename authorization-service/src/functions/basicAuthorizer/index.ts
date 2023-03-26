0b236d27101e68c323407ee5f067fbfe22e7a4dd
Antonina Zinkevich <antonina_zinkevich@epam.com>
2023-03-26 17:26:34 +0300

    fixed description

--- authorization-service/src/functions/basicAuthorizer/index.ts
+++ authorization-service/src/functions/basicAuthorizer/index.ts
@@ -3,5 +3,5 @@ import { handlerPath } from "@libs/handler-resolver";
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
   description:
-    "http authorizer with token type for importProductFile api",
+    "http authorizer with request type for importProductFile api",
 };
