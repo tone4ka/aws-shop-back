1b5bebe052edc6823be53616a524922c81ef2563
Antonina Zinkevich <antonina_zinkevich@epam.com>
2023-03-26 17:09:07 +0300

    hot fix

--- authorization-service/serverless.ts
+++ authorization-service/serverless.ts
@@ -21,7 +21,7 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
-      TONE_4KA: process.env.TONE_4KA,
+      TONE4KA: process.env.TONE4KA,
     },
   },
   functions: { basicAuthorizer1 },
