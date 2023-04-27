93cbbf5b40b5260e791ab3b0864fda85f350805c
Antonina Zinkevich <antonina_zinkevich@epam.com>
2023-04-27 10:56:01 +0300

    added logger

--- import-service/src/libs/lambda.ts
+++ import-service/src/libs/lambda.ts
@@ -1,6 +1,7 @@
 import middy from "@middy/core";
 import middyJsonBodyParser from "@middy/http-json-body-parser";
+import inputOutputLogger from '@middy/input-output-logger'; // !!!!!!!!!!!
 
 export const middyfy = (handler) => {
-  return middy(handler).use(middyJsonBodyParser());
+  return middy(handler).use(middyJsonBodyParser()).use(inputOutputLogger());
 };
